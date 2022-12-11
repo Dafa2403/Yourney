05c5af427697726cdc1c3020a06bcdb4a127a547
dafa <dafadhiyaul24@gmail.com>
2022-12-12 02:43:43 +0700

    copyright

--- src/src/footer.js
+++ src/src/footer.js
@@ -9,12 +9,11 @@ function Footer(){
             <div className='footer'>
                 <IoLogoWhatsapp color='white' size={40}/>
                 <a href='https://www.instagram.com/yourney.project/'><IoLogoInstagram color='white' size={40} /></a>
-                <a href='https://www.instagram.com/yourney.project/'><IoLogoLinkedin color='white' size={40}/></a>
+                <a href='https://www.linkedin.com/in/yourney-project-295325250'><IoLogoLinkedin color='white' size={40}/></a>
                 <IoIosPin color='white' size={40}/>
             </div>
             <div className='cr'>
                 <span>© 2021-2022 Yourney</span>
-                <span>© 2021-2022 Clottania</span>
             </div>
            
         </body>
